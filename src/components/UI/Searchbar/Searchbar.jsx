645024e0b8cd122deafe9e7a3a2d8624c09be546
Mateusz Obloza <mateusz.obloza.0@gmail.com>
2022-06-14 23:24:40 +0200

    style: searchbar & header

--- src/components/UI/Searchbar/Searchbar.jsx
+++ src/components/UI/Searchbar/Searchbar.jsx
@@ -1,4 +1,4 @@
-import { Button, TextField } from "@mui/material";
+import { Box, Button, TextField } from "@mui/material";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -21,18 +21,18 @@ export function Searchbar(props) {
   };
 
   return (
-    <>
+    <Box sx={{ backgroundColor: "#fefb", px: 8, py: 1.3, borderRadius: 3.5 }}>
       <TextField
         value={term}
         onKeyDown={onKeyDownHandler}
         onChange={updateTerm}
         size="small"
         sx={{ mr: 1 }}
-        label="Szukaj..."
+        label="Wyszukaj hotelu..."
       />
       <Button variant="contained" onClick={search}>
         Szukaj
       </Button>
-    </>
+    </Box>
   );
 }
