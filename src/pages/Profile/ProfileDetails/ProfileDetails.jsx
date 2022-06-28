2a93821ea1305c9408d8c6af240cc731fb9e0a81
Mateusz Obloza <mateusz.obloza.0@gmail.com>
2022-06-28 18:11:09 +0200

    feat: connecting profile details to the backend

--- src/pages/Profile/ProfileDetails/ProfileDetails.jsx
+++ src/pages/Profile/ProfileDetails/ProfileDetails.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
-import { Box, TextField, Typography } from "@mui/material";
+import { Box, TextField, Typography, Alert } from "@mui/material";
 import { ButtonLoading } from "../../../components/UI/ButtonLoading/ButtonLoading";
 import { validateEmail } from "../../../helpers/validations";
 import useAuth from "../../../hooks/useAuth";
+import axios from "axios";
 
 export function ProfileDetails(props) {
-  const [auth] = useAuth();
+  const [auth, setAuth] = useAuth();
   const [email, setEmail] = useState(auth.email);
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
@@ -13,16 +14,42 @@ export function ProfileDetails(props) {
     email: "",
     password: "",
   });
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
 
   const buttonDisabled = Object.values(errorMessage).filter((x) => x).length;
 
-  const submit = (e) => {
+  const submit = async (e) => {
     e.preventDefault();
     setLoading(true);
 
-    setTimeout(() => {
-      setLoading(false);
-    }, 500);
+    try {
+      const data = {
+        idToken: auth.token,
+        email: email,
+        returnSecureToken: true,
+      };
+      if (password) {
+        data.password = password;
+      }
+
+      const res = await axios.post(
+        `https://identitytoolkit.googleapis.com/v1/accounts:update?key=${process.env.REACT_APP_SIGN_UP_KEY}`,
+        data
+      );
+
+      setAuth({
+        email: res.data.email,
+        token: res.data.idToken,
+        userId: res.data.localId,
+      });
+      setSuccess(true);
+    } catch (err) {
+      setError(true);
+      setSuccess(false);
+    }
+
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -34,12 +61,12 @@ export function ProfileDetails(props) {
   }, [email]);
 
   useEffect(() => {
-    if (password.length > 4 || !password) {
+    if (password.length > 5 || !password) {
       setErrorMessage({ ...errorMessage, password: "" });
     } else {
       setErrorMessage({
         ...errorMessage,
-        password: "Wymagane co najmniej 5 znaków",
+        password: "Wymagane co najmniej 6 znaków",
       });
     }
   }, [password]);
@@ -60,6 +87,16 @@ export function ProfileDetails(props) {
       <Typography variant="h5" sx={{ mb: 2 }}>
         Edytuj dane logowania
       </Typography>
+      {success ? (
+        <Alert sx={{ mb: 2, width: 370 }}>
+          Dane logowania zostały zmienione
+        </Alert>
+      ) : null}
+      {error ? (
+        <Alert severity="error" sx={{ mb: 2, width: 370 }}>
+          Wystąpił błąd - spróbuj ponownie później
+        </Alert>
+      ) : null}
       <TextField
         type="email"
         value={email}
