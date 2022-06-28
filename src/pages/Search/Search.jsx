552d33cdcd257c3f753364e766bc84f8b155ee92
Mateusz Obloza <mateusz.obloza.0@gmail.com>
2022-06-29 00:08:55 +0200

    fix: searchbar

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,18 +1,38 @@
 import { useParams } from "react-router-dom";
 import { Box, Typography } from "@mui/material";
+import { objectToArray } from "../../helpers/objects";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Hotels } from "../../components/Hotels/Hotels";
 
 export function Search(props) {
   const { term } = useParams();
+  const [hotels, setHotels] = useState([]);
 
-  //   const searchHandler = (term) => {
-  //     const newHotels = [...hotel].filter((x) =>
-  //       x.name.toLowerCase().includes(term.toLowerCase())
-  //     );
-  //   };
+  const searchHandler = async () => {
+    try {
+      const res = await axios.get(
+        `${process.env.REACT_APP_BASE_URL}/hotels.json`
+      );
+
+      const newHotel = objectToArray(res.data).filter((hotel) =>
+        hotel.name.includes(term)
+      );
+
+      setHotels(newHotel);
+    } catch (err) {
+      console.log(err.response);
+    }
+  };
+
+  useEffect(() => {
+    searchHandler();
+  }, [term]);
 
   return (
     <Box>
       <Typography variant="h4">Wyniki dla frazy "{term}": </Typography>
+      <Hotels hotels={hotels} />
     </Box>
   );
 }
