a9cbfc32ade21e1198c53f90ed7453937174a136
Mateusz Obloza <mateusz.obloza.0@gmail.com>
2022-07-11 17:25:08 +0200

    feat: add a choice of number of guests and rooms

--- src/components/UI/Searchbar/Searchbar.jsx
+++ src/components/UI/Searchbar/Searchbar.jsx
@@ -3,13 +3,23 @@
 import HotelIcon from "@mui/icons-material/Hotel";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import PersonIcon from "@mui/icons-material/Person";
-import { AppBar, Box, Button, TextField, Toolbar } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Button,
+  ClickAwayListener,
+  IconButton,
+  TextField,
+  Toolbar,
+} from "@mui/material";
 import { Container } from "@mui/system";
 import styles from "./Searchbar.module.css";
 import { DateRange } from "react-date-range";
 import { useState } from "react";
 import "react-date-range/dist/styles.css";
 import "react-date-range/dist/theme/default.css";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
 import { format } from "date-fns";
 
 export function Searchbar(props) {
@@ -21,6 +31,22 @@ export function Searchbar(props) {
       key: "selection",
     },
   ]);
+  const [openOptions, setOpenOptions] = useState(false);
+  const [options, setOptions] = useState({
+    adult: 2,
+    children: 0,
+    room: 1,
+  });
+
+  const handleOption = (name, operation) => {
+    setOptions((prev) => {
+      return {
+        ...prev,
+        [name]:
+          operation === "increase" ? options[name] + 1 : options[name] - 1,
+      };
+    });
+  };
 
   return (
     <Container>
@@ -40,23 +66,96 @@ export function Searchbar(props) {
               "dd/MM/yyyy"
             )}`}</span>
             {openDate && (
-              <DateRange
-                editableDateInputs={true}
-                onChange={(item) => setDate([item.selection])}
-                moveRangeOnFirstSelection={false}
-                ranges={date}
-                className={styles.date}
-              />
+              <ClickAwayListener onClickAway={() => setOpenDate(false)}>
+                <DateRange
+                  editableDateInputs={true}
+                  onChange={(item) => setDate([item.selection])}
+                  moveRangeOnFirstSelection={false}
+                  ranges={date}
+                  className={styles.date}
+                />
+              </ClickAwayListener>
             )}
           </Box>
           <Box className={styles.searchItem}>
             <PersonIcon className={styles.icon} />
-            <span className={styles.span}>
-              2 dorosłych · 0 dzieci · 1 pokój
+            <span
+              onClick={() => setOpenOptions(!openOptions)}
+              className={styles.span}
+            >
+              {`${options.adult}`}{" "}
+              {options.adult === 1 ? "dorosły" : "dorosłych"} ·{" "}
+              {`${options.children}`}{" "}
+              {options.children === 1 ? "dziecko" : "dzieci"} ·{" "}
+              {`${options.room}`} {options.room === 1 ? "pokój" : "pokoje"}
             </span>
+            {openOptions && (
+              <ClickAwayListener onClickAway={() => setOpenOptions(false)}>
+                <Box className={styles.options}>
+                  <Box className={styles.optionItem}>
+                    <span className={styles.optionText}>Dorosli</span>
+                    <Box className={styles.optionCounter}>
+                      <IconButton
+                        disabled={options.adult <= 1}
+                        onClick={() => handleOption("adult", "decrease")}
+                      >
+                        <RemoveIcon />
+                      </IconButton>
+                      <span className={styles.optionCounterNumber}>
+                        {options.adult}
+                      </span>
+                      <IconButton
+                        onClick={() => handleOption("adult", "increase")}
+                      >
+                        <AddIcon />
+                      </IconButton>
+                    </Box>
+                  </Box>
+                  <Box className={styles.optionItem}>
+                    <span className={styles.optionText}>Dzieci</span>
+                    <Box className={styles.optionCounter}>
+                      <IconButton
+                        disabled={options.children <= 0}
+                        onClick={() => handleOption("children", "decrease")}
+                      >
+                        <RemoveIcon />
+                      </IconButton>
+                      <span className={styles.optionCounterNumber}>
+                        {options.children}
+                      </span>
+                      <IconButton
+                        onClick={() => handleOption("children", "increase")}
+                      >
+                        <AddIcon />
+                      </IconButton>
+                    </Box>
+                  </Box>
+                  <Box className={styles.optionItem}>
+                    <span className={styles.optionText}>Pokoje</span>
+                    <Box className={styles.optionCounter}>
+                      <IconButton
+                        disabled={options.room <= 1}
+                        onClick={() => handleOption("room", "decrease")}
+                      >
+                        <RemoveIcon />
+                      </IconButton>
+                      <span className={styles.optionCounterNumber}>
+                        {options.room}
+                      </span>
+                      <IconButton
+                        disabled={options.room >= 4}
+                        onClick={() => handleOption("room", "increase")}
+                      >
+                        <AddIcon />
+                      </IconButton>
+                    </Box>
+                  </Box>
+                </Box>
+              </ClickAwayListener>
+            )}
           </Box>
           <Box className={styles.searchItem}>
-            <Button variant="contained" color="success">
+            <Button variant="contained" color="success" sx={{ ml: 3 }}>
               Szukaj
             </Button>
           </Box>
